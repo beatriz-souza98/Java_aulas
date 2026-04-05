0936a6a597ab12cefa3382a92b183194b8e2cbc0
beatriz-souza98 <beatrizsouza231998@gmail.com>
2026-04-05 14:19:03 -0300

    IfElse

---
+++ src/EstruturaIFElse.java
@@ -0,0 +1,37 @@
+public class EstruturaIFElse {
+
+    public static void main(String[] args) {
+
+//        int idade = 30;
+//
+//        if (idade > 20){
+//            System.out.println("Você é maior de idade");
+//        }
+//        else{
+//            System.out.println("Você é menor de idade");
+//        }
+//        }
+//
+//        boolean estaChovendo = true;
+//
+//        if (!estaChovendo){
+//            System.out.println("Vamos sair para passear");
+//        }
+//        else {
+//            System.out.println("Vamos ficar em casa");
+//        }
+//
+        boolean tempix = false;
+        boolean temcartao = false;
+
+        if (tempix && temcartao){
+            System.out.println("Pode pedir almoço e uma sobremesa");
+        } else if (tempix || temcartao) {
+            System.out.println(" Compre só o ifood");
+        }
+        else {
+            System.out.println("Compre nada, tu é lisa");
+        }
+    }
+    }
+
