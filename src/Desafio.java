514b40b07bad1ae6df75bb9fa32ca49f44e9039d
beatriz-souza98 <beatrizsouza231998@gmail.com>
2026-04-05 18:11:25 -0300

    Estruturas Repetitivas

---
+++ src/Desafio.java
@@ -0,0 +1,26 @@
+public class Desafio {
+
+    public static void main(String[] args) {
+
+//        Crie um sistema que:
+//
+//        Tenha um array com 5 números
+//        Use for para percorrer
+//        Use if para separar pares e ímpares
+//        Mostre:
+//        Quantos pares
+//        Quantos ímpares
+
+        int[] numeros = {1, 2, 3, 4, 5};
+
+
+        for (int contador = 0; contador < numeros.length; contador++) {
+            if (numeros[contador] % 2 == 0){
+                System.out.println("O número " + numeros[contador] + "  é par");
+            }
+            else {
+                System.out.println("O número " + numeros[contador] + "  é impar");
+            }
+        }
+    }
+}
