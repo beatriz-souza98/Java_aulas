272b1b240f4bc995e17592c095bb2e4e36d542ba
beatriz-souza98 <beatrizsouza231998@gmail.com>
2026-04-05 22:43:32 -0300

    Hash Map

---
+++ src/Desafio3.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+
+public class Desafio3 {
+    public static void main(String[] args) {
+
+        //Criar uma lista de produtos com preço
+
+        HashMap<String, Double> listaDeCompras = new HashMap<>();
+        listaDeCompras.put("Arroz", 10.90);
+        listaDeCompras.put("Carne", 39.90);
+        listaDeCompras.put("Leite", 3.99);
+
+        for (String produto : listaDeCompras.keySet()){
+            double preco = listaDeCompras.get(produto);
+
+            System.out.println(produto + " está custando: " + preco);
+        }
+    }
+}
