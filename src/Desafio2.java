d91eb4209a6708403341404b50d3ca2d9cb67ad0
beatriz-souza98 <beatrizsouza231998@gmail.com>
2026-04-05 22:20:00 -0300

    Concluido

--- src/Desafio2.java
+++ src/Desafio2.java
@@ -3,7 +3,10 @@ import java.util.List;
 
 public class Desafio2 {
     public static void main(String[] args) {
-        //Utilizando Array List
+        //Crie uma lista de nomes e mostre:
+        //Todos os nomes
+        //Quantos nomes tem na lista
+
         List<String> nomes = new ArrayList<>();
         nomes.add("Ana");
         nomes.add("João");
@@ -12,7 +15,6 @@ public class Desafio2 {
         for (String nome : nomes) {
             System.out.println(nome);
         }
-
         System.out.println("Tamanho da ArrayList é: " + nomes.size());
     }
 }
