6778d0444e3ee85c5267c10417212d88d71ae1b8
beatriz-souza98 <beatrizsouza231998@gmail.com>
2026-04-06 22:32:28 -0300

    1.1 0k

---
+++ src/exercicios_mentoria.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class exercicios_mentoria {
+    public static void main(String[] args) {
+
+//    1.1 Área do Quadrado - v1
+//    Calcular e exibir a área de um quadrado, a partir do valor de sua aresta que será digitado.
+//    A fórmula para calcular a área de um quadrado é A = L2, onde L é a medida do lado do quadrado.
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite aqui o lado do quadrado: ");
+        double lado = scanner.nextDouble();
+        double area = lado * lado;
+        System.out.println("A área do quadrado é: " + area);
+
+//        1.2 Volume da Esfera
+//        Calcular e exibir o volume de uma esfera a partir do valor de seu diâmetro que será digitado.
+//
+//                Como calcular o volume da esfera
+//
+//        1.3 Média Aritmética
+//
+//        Calcular e exibir a média aritmética de quatro valores quaisquer que serão digitados.
+//                Como calcular a média
+//
+//        1.4 Milhas vs Km
+//
+//        Sabendo que uma milha marítima equivale a um mil, oitocentos e cinqüenta e dois metros e que um
+//        quilômetro possui mil metros, fazer um programa para converter milhas marítimas em quilômetros.
+
+    }
+}
