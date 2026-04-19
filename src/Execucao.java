08bed26473ebb7d0febe3a822de4476cc31596f9
beatriz-souza98 <beatrizsouza231998@gmail.com>
2026-04-19 17:23:08 -0300

    Metódos de Instâncias e Estáticos

---
+++ src/Execucao.java
@@ -0,0 +1,9 @@
+public class Execucao {
+    public static void main(String[] args) {
+
+        Integer cpf = 125487;
+
+        String cpfConvertido = Util.conversorString(cpf);
+        System.out.println(cpfConvertido);
+    }
+}
