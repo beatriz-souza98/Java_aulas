ac6c9d91d11c1abdcfb36aaa0d749e1c12a48349
beatriz-souza98 <beatrizsouza231998@gmail.com>
2026-04-19 14:09:07 -0300

    Objeto Casa

---
+++ src/Casa.java
@@ -0,0 +1,15 @@
+public class Casa {
+    public static void main(String[] args) {
+        PlantaCasa casa = new PlantaCasa();
+
+        casa.numeroQuartos = 3;
+        casa.numeroBanheiros = 2;
+        casa.metragem = 70;
+        casa.cor = "Cinza";
+        casa.material = "Tijolo";
+
+        casa.construir();
+        casa.pintar();
+
+    }
+}
---
+++ src/PlantaCasa.java
@@ -0,0 +1,19 @@
+public class PlantaCasa {
+
+    int metragem;
+    int numeroQuartos;
+    int numeroBanheiros;
+    String cor;
+    String material;
+
+    public void construir(){
+        System.out.println("Metragem " + metragem);
+        System.out.println("Numeros de Quartos " + numeroQuartos);
+        System.out.println("Numeros de " + numeroBanheiros);
+        System.out.println("Material " + material);
+    }
+
+    public void pintar(){
+        System.out.println("Cor da casa  " + cor);
+    }
+}
