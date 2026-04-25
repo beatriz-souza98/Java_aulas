57517b12dbe6d6480ab0295335cdbe1008c6c359
beatriz-souza98 <beatrizsouza231998@gmail.com>
2026-04-25 18:59:02 -0300

    Sobrecarga de Metodos

---
+++ src/Polimorfismo/Main.java
@@ -0,0 +1,21 @@
+package Polimorfismo;
+
+public class Main {
+    public static void main(String[] args) {
+        int sabaoPo = 2;
+        int mamao = 5;
+        double sabonete = 2.90;
+        double arroz = 5.90;
+        Integer batata = 6;
+        Integer cenoura = 7;
+
+        int total = Calculadora.somar(sabaoPo, mamao);
+        double totalDouble = Calculadora.somar(sabonete, arroz);
+        Integer totalInteger = Calculadora.somar(batata, cenoura);
+
+        System.out.println(total);
+        System.out.println(totalDouble);
+        System.out.println(totalInteger);
+
+    }
+}
