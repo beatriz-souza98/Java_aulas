b4a6eeafd39ee235a1f16e059ac9650be70639a3
beatriz-souza98 <beatrizsouza231998@gmail.com>
2026-05-07 12:36:31 -0300

    IOException e BufferReader

--- src/exececao/Verificada/LeitorDocumentos.java
+++ src/exececao/Verificada/LeitorDocumentos.java
@@ -1,4 +1,25 @@
 package exececao.Verificada;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class LeitorDocumentos {
-}
+    public static void main(String[] args) {
+        BufferedReader bufferedReader = null;
+        try {
+            bufferedReader = new BufferedReader(
+                    new FileReader("C:\\Users\\Marques Adriel\\Documents\\texto.txt"));
+            String linha;
+
+            while ((linha = bufferedReader.readLine()) != null){
+                System.out.println(linha);
+        }
+        } catch (IOException e) {
+            System.out.println("Erro ao ler arquivo" + e.getMessage());
+            throw new RuntimeException(e);
+        }
+
+        }
+    }
